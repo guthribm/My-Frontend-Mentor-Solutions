6412bc951c887be7a376751bf5ab05b4f369979d
Brandon Guthrie <guthribm@gmail.com>
2022-01-22 23:14:29 -0600

    adds javascript to custom tip button, modal and overlay, and and handlers

--- challenge pages/tip-calculator-app-main/index.js
+++ challenge pages/tip-calculator-app-main/index.js
@@ -1,3 +1,6 @@
+let overlay = document.getElementById("overlay");
+let submitBtn = document.getElementById("submit-btn");
+let customTip = document.getElementById("custom-tip");
 let myBill = document.getElementById("bill");
 let numOfPeople = document.getElementById("num-ppl");
 let five = document.getElementById("five");
@@ -5,12 +8,14 @@ let ten = document.getElementById("ten");
 let fifteen = document.getElementById("fifteen");
 let twentyFive = document.getElementById("twenty-five");
 let fifty = document.getElementById("fifty");
+let custom = document.getElementById("custom");
 let tipPer = document.getElementById("tip-amount");
 let totalPlusTip = document.getElementById("total-plus-tip");
 let resetBtn = document.getElementById("reset-btn");
 
 let billTotal;
 let totalPeople;
+let customTipEntered;
 
 function handleInput(event) {
   billTotal = event.target.value;
@@ -26,12 +31,25 @@ myBill.addEventListener("input", handleInput);
 numOfPeople.addEventListener("input", handlePeople);
 
 function calculateTotals(tip) {
-  console.log(`calc function: ${this}`);
   tipPer.textContent = "$" + ((billTotal / totalPeople) * tip).toFixed(2);
   totalPlusTip.textContent =
     "$" +
     (billTotal / totalPeople + (billTotal / totalPeople) * tip).toFixed(2);
 }
+
+function handleCustomTip(event) {
+  customTipEntered = event.target.value;
+  console.log(`handleCustomTip: ${event.target.value}`);
+}
+
+customTip.addEventListener("input", handleCustomTip);
+
+function handleTip() {
+  calculateTotals(customTipEntered * 0.01);
+  overlay.style.visibility = "hidden";
+}
+
+submitBtn.addEventListener("click", handleTip);
 
 function resetFeilds() {
   tipPer.textContent = "$0.00";
@@ -40,6 +58,11 @@ function resetFeilds() {
   numOfPeople.value = "";
   billTotal = 0;
   totalPeople = 0;
+  customTipEntered = 0;
+}
+
+function customModal() {
+  overlay.style.visibility = "visible";
 }
 
 five.addEventListener("click", () => calculateTotals(0.05));
@@ -47,5 +70,6 @@ ten.addEventListener("click", () => calculateTotals(0.1));
 fifteen.addEventListener("click", () => calculateTotals(0.15));
 twentyFive.addEventListener("click", () => calculateTotals(0.25));
 fifty.addEventListener("click", () => calculateTotals(0.5));
+custom.addEventListener("click", customModal);
 
 resetBtn.addEventListener("click", resetFeilds);
